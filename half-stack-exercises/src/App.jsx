d9a45dc7f8ac2e0a091fde686792a62cbe682187
Hansespinosa2 <133429105+Hansespinosa2@users.noreply.github.com>
2025-08-25 03:27:54 -0400

    Complete exercises 1.3-1.5

--- half-stack-exercises/src/App.jsx
+++ half-stack-exercises/src/App.jsx
@@ -14,36 +14,43 @@ function Part(props) {
 
 function Content(props) {
   return (
-    <>
-      {props.parts.map((part, index) => (
-        <Part part={part} exercise={props.exercises[index]} />
-      ))}
-    </>
+      <>
+        {props.parts.map(part => (
+          <Part key={part.name} part={part.name} exercise={part.exercises} />
+        ))}
+      </>
   );
 }
 
 function Footer(props) {
+  const number_of_exercises = props.number_of_exercises.reduce((sum, part) => sum + part.exercises, 0)
   return (
-    <p>Number of exercises: {props.number_of_exercises}</p>
+    <p>Number of exercises: {number_of_exercises}</p>
   )
 }
 
 function App() {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const parts = [part1, part2, part3]
-  const exercises = [exercises1, exercises2, exercises3]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises:  10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+  }]}
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} exercises={exercises} />
-      <Footer number_of_exercises={exercises1 + exercises2 + exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Footer number_of_exercises={course.parts} />
     </div>
   )
 }

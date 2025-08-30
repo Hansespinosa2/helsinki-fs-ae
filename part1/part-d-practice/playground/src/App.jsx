64af0abd527115dc4445dd63790bc038923b8675
Hansespinosa2 <133429105+Hansespinosa2@users.noreply.github.com>
2025-08-30 01:20:44 -0400

    Read through part d before exercises

--- part1/part-d-practice/playground/src/App.jsx
+++ part1/part-d-practice/playground/src/App.jsx
@@ -1,34 +1,76 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+const History = ({ allClicks }) => {
+  if (allClicks.length == 0) {
+    // it would be so haram to define a react hook like useState or useEffect here since it is in a conditional
+    return (
+      <div>
+        Use the app by pressing the left or right button
+      </div>
+    )
+  }
+  return (
+    <div>Button press history: {allClicks.join(' ')}</div>
+  )
+}
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [left, setLeft] = useState(0)
+  // const [right, setRight] = useState(0)
+
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+
+  const [allClicks, setAll] = useState([])
+
+  const [totalClicks, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    const increasedLeft = clicks.left + 1
+    const newClicks = {
+      left: increasedLeft,
+      right: clicks.right
+    }
+    setClicks(newClicks)
+    setAll(allClicks.concat('L'))
+    setTotal(clicks.right + increasedLeft )
+  }
+  const handleRightClick = () => {
+    const increasedRight = clicks.right + 1
+    const newClicks = {
+      left: clicks.left ,
+      right: increasedRight
+    }
+    setClicks(newClicks)
+    setAll(allClicks.concat('R'))
+    setTotal(increasedRight + clicks.left)
+  }
+
+  const hello = () => {
+    const handler = () => console.log("Hello world")
+    return handler
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {clicks.left}
+      <Button handleClick={handleLeftClick} text={"left"} />
+      <Button handleClick={handleRightClick} text={"right"} />
+      {clicks.right}
+      <History allClicks={allClicks}/>
+      <p>Total: {totalClicks}</p>
+      <Button handleClick={() => console.log("AHHHHHHHHHHH")} text="AHHHHHHHHHHHhh" />
+      <Button handleClick={hello()} text="Hello world" />
+    </div>
   )
 }
 
